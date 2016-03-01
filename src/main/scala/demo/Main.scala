928466df181971fa6905069229c434750d681061
Tim Harper <timcharper@gmail.com>
2016-03-01 00:41:23 -0700

    7) Output dropped messages

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,24 +1,38 @@
 package demo
 
+import scala.concurrent.Future
+import scala.util.Success
 import Twitter.tweetFormat
-import akka.actor.{ActorRef, ActorSystem}
-import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.{QueueOfferResult, ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl._
 import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
 import play.api.libs.json._
-import scala.concurrent.Future
 
 object Main extends App {
   implicit val as = ActorSystem("hi")
   implicit val materializer = ActorMaterializer()
   import as.dispatcher
 
+  def droppingBuffer[T](size: Int, strategy: OverflowStrategy = OverflowStrategy.backpressure):
+      Flow[T, T, NotUsed] =
+    Flow[T].conflate({ (first, second) =>
+      println(s"Bummer! Dropping ${second}")
+      first
+    }).buffer(size, strategy)
+
   val tweetSource: Source[Tweet, () => Unit] =
     Source.queue[Tweet](5, OverflowStrategy.dropNew).
       mapMaterializedValue { input =>
         println("Start")
         Twitter.subscribePubnubChannel("pubnub-twitter") { tweet =>
-          input.offer(tweet)
+          input.offer(tweet).onComplete {
+            case Success(QueueOfferResult.Enqueued) =>
+              ()
+            case _ =>
+              println(s"Bummer! ${tweet} dropped")
+          }
         }
 
         { () => input.complete() }
@@ -27,8 +41,10 @@ object Main extends App {
   val sockets: Source[IncomingConnection, Future[ServerBinding]] =
     Tcp().bind("0.0.0.0", 8888)
 
+  val pipeline = tweetSource.
+    zip(sockets)
 
-  tweetSource.zip(sockets).runForeach { case (tweet, socket) =>
+  pipeline.runForeach { case (tweet, socket) =>
     socket.handleWith(Helpers.writing(Json.toJson(tweet).toString + "\n"))
   }
 
