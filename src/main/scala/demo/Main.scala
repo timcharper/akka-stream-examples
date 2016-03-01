e0e345b10b3ba2e32176e94cf6058940334dc977
Tim Harper <timcharper@gmail.com>
2016-03-01 10:06:45 -0700

    2) Introduce async boundary / fusing

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -10,83 +10,28 @@ object Main extends App {
   implicit val as = ActorSystem("hi")
   implicit val materializer = ActorMaterializer()
 
-  /*
-   There are three primary primitive shapes in Akka Streams
-
-   - Source
-   - Flow
-   - Sink
-   */
-  /* Sources have a closed head, but open tail
-
-   +--------
-   | SOURCE
-   +--------
-
-   */
-  val numbers = Source(List(1,2,3))
-
-
-  /*
-   Flows have open ends
-
-   ------
-    FLOW
-   ------
-
-   */
-  val increment = Flow[Int].map(_ + 1)
-
-  /* Sinks are the opposite of sources
-
-   ------+
-    SINK |
-   ------+
-   */
-  val sink = Sink.foreach(println)
+  println("Starting\n")
+  Source(List(1,2,3)).
+    map { n =>
+      println(s"${n}     ")
+      Thread.sleep(500)
+      println(s" ${n}    ")
+      n
+    }.
+    // async.
+    map { n =>
+      println(s"  ${n}   ")
+      Thread.sleep(500)
+      println(s"   ${n}  ")
+      n
+    }.
+    runForeach { n =>
+      println(s"     ${n}!")
+    }
 
 
-  /*
-   +--------   ------    +-----------------------------
-   | SOURCE  +  FLOW   = | SOURCE <with flow behavior>
-   +--------   ------    +-----------------------------
-   */
-  val incrementedSource: Source[Int, NotUsed] =
-    numbers.via(increment)
 
-  /*
-   ------   ------+    ----------------------+
-    FLOW  +  SINK |  =  <flow behavior> SINK |
-   ------   ------+    ----------------------+
-   */
-  val incrementingSink: Sink[Int, NotUsed] =
-    increment.to(sink)
 
-  /* Combine the parts together to close the graph
-   +--------   ------   ------+   +---------------+
-   | SOURCE  +  FLOW  +  SINK | = | RunnableGraph |
-   +--------   ------   ------+   +---------------+
-   */
-  val graph: RunnableGraph[NotUsed] =
-    numbers.via(increment).to(sink)
 
-  // Nothing has happened yet! Once the graph is closed, lets run it
-  graph.run()
-
-  /* More shapes exist to help build complete graph topology
-
-   - Fan out
-   - Fan in
-   - Routers
-   - Bidirectional flows (it's like a "higher-kinded" flow)
-
-      BIDI    +    Flow   =    Flow
-   ==========   =========   ============
-    <--out--     --out-\
-                       |     in --> out
-     --in-->     --in--/
-   ==========   =========   ============
-
-   */
 }
 
