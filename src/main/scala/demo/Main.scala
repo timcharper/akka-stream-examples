4073848aa02307c876839b384dec220e45078de2
Tim Harper <timcharper@gmail.com>
2016-03-01 00:24:34 -0700

    3) Simple pubnub (no streams in this one)

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,37 +1,9 @@
 package demo
 
-import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl._
-
 object Main extends App {
-
-  implicit val as = ActorSystem("hi")
-  implicit val materializer = ActorMaterializer()
-
-  println("Starting\n")
-  Source(List(1,2,3)).
-    map { n =>
-      println(s"${n}     ")
-      Thread.sleep(500)
-      println(s" ${n}    ")
-      n
-    }.
-    // async.
-    map { n =>
-      println(s"  ${n}   ")
-      Thread.sleep(500)
-      println(s"   ${n}  ")
-      n
-    }.
-    runForeach { n =>
-      println(s"     ${n}!")
-    }
-
-
-
-
-
+  // Simple data source
+  Twitter.subscribePubnubChannel("pubnub-twitter") { t =>
+    println(t)
+  }
 }
 
