7d64a420a3ddd94b1a2405daacfe7e1c8ea3e789
Tim Harper <timcharper@gmail.com>
2016-03-01 00:27:04 -0700

    4) Ingest pubnum into Akka stream to produce a tweet Source

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,9 +1,26 @@
 package demo
 
+import akka.actor.{ActorRef, ActorSystem}
+import akka.stream.{OverflowStrategy,ActorMaterializer}
+import akka.stream.scaladsl._
+
 object Main extends App {
-  // Simple data source
-  Twitter.subscribePubnubChannel("pubnub-twitter") { t =>
-    println(t)
-  }
+  implicit val as = ActorSystem("hi")
+  implicit val materializer = ActorMaterializer()
+  import as.dispatcher
+
+  val tweetSource: Source[Tweet, () => Unit] =
+    Source.queue[Tweet](5, OverflowStrategy.dropNew).
+      mapMaterializedValue { input =>
+        // Does not print until code is run
+        println("Start")
+        Twitter.subscribePubnubChannel("pubnub-twitter") { tweet =>
+          input.offer(tweet)
+        }
+
+        { () => input.complete() }
+      }
+
+  // tweetSource.runForeach(println)
 }
 
